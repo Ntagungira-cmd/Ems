3a5be61b8631b233e3ea8d6ee9596fd463f6d89b
ntagungira-cmd <ntagungiraali@gmail.com>
2022-06-06 21:21:38 +0200

    pagination

--- src/main/java/com/stormtech/demo/controllers/EmployeeController.java
+++ src/main/java/com/stormtech/demo/controllers/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.stormtech.demo.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +24,7 @@ public class EmployeeController {
 
 	@GetMapping("/")
 	public String viewHomePage(Model model) {
-		model.addAttribute("listEmployees", employeeService.getAllEmployees());
-		return "index";
+		return findPaginated(1, model);
 	}
 
 	@GetMapping("/showNewEmployeeForm")
@@ -59,4 +61,17 @@ public class EmployeeController {
 		return "redirect:/";
 	}
 
+	@GetMapping("/page/{pageNo}")
+	public String findPaginated(@PathVariable(value = "pageNo") int pageNo, Model model) {
+		int pageSize = 5;
+
+		Page<Employee> page = employeeService.findPaginated(pageNo, pageSize);
+		List<Employee> listEmployees = page.getContent();
+
+		model.addAttribute("currentPage", pageNo);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listEmployees", listEmployees);
+		return "index";
+	}
 }
